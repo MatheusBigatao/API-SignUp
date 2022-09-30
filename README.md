debf5a67bb43d6237dee69d8d65cbba1425768be
matheusbigatao <matheus.bmartinelli@gmail.com>
2022-09-30 20:03:44 -0300

    chore: add ts-jest

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Tecnologias
+## Technologies
 - JavaScript
 - TypeScript
 - Jest
--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
   roots: ['<rootDir>/src'],
   // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
@@ -7,16 +7,16 @@ export default {
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  coverageDirectory: 'coverage',
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8",
+  coverageProvider: 'v8',
 
   // The test environment that will be used for testing
-  testEnvironment: "node",
+  testEnvironment: 'node',
 
   // A map from regular expressions to paths to transformers
   transform: {
     '.+\\.ts$': 'ts-jest'
-  },
-};
+  }
+}
